6561e659e48ad99bac69e640924df5f9c6d7a0b4
Spencer Wright <swright41n@gmail.com>
2025-08-01 23:36:30 -0600

    files upload

---
+++ RenoSystem/RenoSystemExtentions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using RenoSystem.DAL;
+using RenoSystem.BLL;
+
+namespace RenoSystem
+{
+    public static class RenoSystemExtensions
+    {
+        public static IServiceCollection RenoSystemExtensionServices(this IServiceCollection services, string connection)
+        {
+            services.AddDbContext<RenosContext>(options => options.UseSqlServer(connection));
+            services.AddTransient<JobServices>();
+            services.AddTransient<SupplyServices>();
+            return services;
+        }
+    }
+}
